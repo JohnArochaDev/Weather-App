d19ae067015f1162ee52ca6af2376d53ad441f10
John Arocha Dev <jparocha777@gmail.com>
2023-12-28 15:31:37 -0500

    new system to impliment data

--- controllers/weatherControllers.js
+++ controllers/weatherControllers.js
@@ -18,31 +18,18 @@ let lat = 42.3761504
 let long = -85.45500539999999
 
 // TEST COORDS     42.3761504,-85.45500539999999
+
 //////////////////////////////
 ///  Routes + Controllers ////
 //////////////////////////////
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
-// This will be for the single day
+
+// This will be for the hourly forcasts
 router.get('/weather/daily', (req, res) => {
-    const time = []
-    const temp = []
-    const day = []
-    const wind = []
-    const precip = []
-    const snow = []
-    const humidity = []
-    const cloud = []
-    const feelslike = []
-    const windchill = []
-    const heatindex = []
-    const dewpoint = []
-    const chanceOfRain = []
-    const chanceOfSnow = []
-    const visability = []
-    const uv = []
     axios(`${weatherURL}${lat},${long}&days=3&aqi=yes&alerts=no`) //                            Everything will be inside of apiRes.data!!!!!!!!!!!
         // render the results on a 'show' page: aka 'detail' page
         .then(apiRes => {
@@ -51,47 +38,58 @@ router.get('/weather/daily', (req, res) => {
             let locName = weather.location.name
             let forecast1 = weather.forecast.forecastday[0] // DAY OF
             let forecast2 = weather.forecast.forecastday[1] // TOMORROW
-            let forecast3 = weather.forecast.forecastday[2] // TOMORROW'S TOMORROW
             let locRegion = weather.location.region
             let locTime = weather.location.localtime
-            let locDate = forecast1.date
-            let maxTemp = forecast1.day.maxtemp_f
-            let minTemp = forecast1.day.mintemp_f
-            let avrTemp = forecast1.day.avgtemp_f
-            let maxWind = forecast1.day.maxwind_mph
-            let totalPrecip = forecast1.day.totalprecip_in
-            let totalSnow = forecast1.day.totalsnow_cm
-            let localHumidity = forecast1.day.avghumidity
-            let rainChance = forecast1.day.daily_chance_of_rain
-            let snowChance = forecast1.day.daily_chance_of_snow
-            let condition = forecast1.day.condition.text
-            let conditionIcon = forecast1.day.condition.icon
 
-            let hour = forecast1.hour // hour us an array I need to sift through with for each and hour.length
-            // I need to write a function that uses the hour data from locTime, and uses splice() to copy the array starting at the point from locTime, and the make the below forEach iterate through the new array. This will allow me to display the hourly data ONLY for the future, not the whole day.
+            let hours = forecast1.hour
+
+            // THIS IS THE LIST OF DATA AND ITS ENDPOINTS FOR THE EJS
+            // let weather = apiRes.data
+            // let locName = weather.location.name
+            // let forecast1 = weather.forecast.forecastday[0] // DAY OF
+            // let forecast2 = weather.forecast.forecastday[1] // TOMORROW
+            // let forecast3 = weather.forecast.forecastday[2] // TOMORROW'S TOMORROW
+            // let locRegion = weather.location.region
+            // let locTime = weather.location.localtime
+            // let locDate = forecast1.date
+            // let maxTemp = forecast1.day.maxtemp_f
+            // let minTemp = forecast1.day.mintemp_f
+            // let avrTemp = forecast1.day.avgtemp_f
+            // let maxWind = forecast1.day.maxwind_mph
+            // let totalPrecip = forecast1.day.totalprecip_in
+            // let totalSnow = forecast1.day.totalsnow_cm
+            // let localHumidity = forecast1.day.avghumidity
+            // let rainChance = forecast1.day.daily_chance_of_rain
+            // let snowChance = forecast1.day.daily_chance_of_snow
+            // let condition = forecast1.day.condition.text
+            // let conditionIcon = forecast1.day.condition.icon
+
+            // THis will be the time we start from
             let localTime = locTime[11] + locTime[12]
             localTime ++
-            let hourTime = hour.splice(localTime)
-            hourTime.forEach((data) => {
-                time.push(data.time)
-                temp.push(data.temp_f)
-                day.push(data.is_day)
-                wind.push(data.wind_mph)
-                precip.push(data.precip_in)
-                snow.push(data.snow_cm)
-                humidity.push(data.humidity)
-                cloud.push(data.cloud)
-                feelslike.push(data.feelslike_f)
-                windchill.push(data.windchill_f)
-                heatindex.push(data.heatindex_f)
-                dewpoint.push(data.dewpoint_f)
-                chanceOfRain.push(data.chance_of_rain)
-                chanceOfSnow.push(data.chance_of_snow)
-                visability.push(data.vis_miles)
-                uv.push(data.uv)
-            })
-            res.render('weather/daily', { locName, locRegion, locTime, locDate, maxTemp, minTemp, avrTemp, maxWind, totalPrecip, totalSnow, localHumidity, rainChance, snowChance, condition, conditionIcon, time, temp, day, wind, precip, snow, humidity, cloud, feelslike, 
-                windchill, heatindex, dewpoint, chanceOfRain, chanceOfSnow, visability, uv})
+            let hourTime = hours.splice(localTime)
+            while (hourTime.length < 12) {
+                let idx = 0;
+                hourTime.push(forecast2.hour[idx])
+                idx++
+            }
+            console.log('This is the list starting point after: \n', hourTime)
+            
+            let hour = hourTime[0]
+            let hour1 = hourTime[1]
+            let hour2 = hourTime[2]
+            let hour3 = hourTime[3]
+            let hour4 = hourTime[4]
+            let hour5 = hourTime[5]
+            let hour6 = hourTime[6]
+            let hour7 = hourTime[7]
+            let hour8 = hourTime[8]
+            let hour9 = hourTime[9]
+            let hour10 = hourTime[10]
+            let hour11 = hourTime[11]
+            //TESTING
+            res.render('weather/daily', { hour, hour1, hour2, hour3, hour4, hour5, hour6, hour7, hour8, hour9, hour10, hour11})
+            //TESTING
         })
         .catch(err => {
             console.log('error', err)
