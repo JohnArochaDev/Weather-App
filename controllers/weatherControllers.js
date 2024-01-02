d2c39cc3b2b971daf620d34069a50cf5eaf474a3
John Arocha Dev <jparocha777@gmail.com>
2024-01-02 10:28:59 -0500

    The google maps is working to make a get request

--- controllers/weatherControllers.js
+++ controllers/weatherControllers.js
@@ -177,18 +177,11 @@ router.get('/', async function(req, res, next) {
 })
 
 
-
-
-
-
-
-
-
-
 // This will be for the hourly forcasts with Lattitude and Longitude data !!!!!!!!!!!!!!!!!!!!!!!
-router.get('/weather/daily', (req, res) => {
+router.get('/weather/daily/:coords', (req, res) => {
     const { username, loggedIn, userId } = req.session
-    axios(`${weatherURL}${lat},${long}&days=3&aqi=yes&alerts=no`) //                            Everything will be inside of apiRes.data!!!!!!!!!!!
+    let coords = req.params.coords
+    axios(`${weatherURL}${coords}&days=3&aqi=yes&alerts=no`) //                            Everything will be inside of apiRes.data!!!!!!!!!!!
         // render the results on a 'show' page: aka 'detail' page
         .then(apiRes => {
             let weather = apiRes.data
@@ -420,135 +413,242 @@ router.get('/weather/daily', (req, res) => {
 })
 
 
-router.get('/', async function(req, res, next) {
+
+
+// This will be for the hourly forcasts with Lattitude and Longitude data !!!!!!!!!!!!!!!!!!!!!!!
+router.get('/weather/daily', (req, res) => {
     const { username, loggedIn, userId } = req.session
-    await axios(`${weatherURL}${newYork}&days=1&aqi=yes&alerts=no`) //                            Everything will be inside of apiRes.data!!!!!!!!!!!
-    // render the results on a 'show' page: aka 'detail' page
-    .then(apiRes => {
-        let weather = apiRes.data
-        let locName = weather.location.name
-        homelocName.push(locName)
-        let tempF = weather.current.temp_f
-        homeTemp.push(tempF)
-        let condition = weather.current.condition.text
-        homeCondition.push(condition)
-        // console.log('1 This is the Home Name: \n', homelocName,'1 This is the Temp Name: \n', homeTemp,'1 This is the Cindition Name: \n', homeCondition)
-    })
-    await axios(`${weatherURL}${losAngeles}&days=1&aqi=yes&alerts=no`) //                            Everything will be inside of apiRes.data!!!!!!!!!!!
-    // render the results on a 'show' page: aka 'detail' page
-    .then(apiRes => {
-        let weather = apiRes.data
-        let locName = weather.location.name
-        homelocName.push(locName)
-        let tempF = weather.current.temp_f
-        homeTemp.push(tempF)
-        let condition = weather.current.condition.text
-        homeCondition.push(condition)
-        // console.log('2 This is the Home Name: \n', homelocName,'2 This is the Temp Name: \n', homeTemp,'2 This is the Cindition Name: \n', homeCondition)
-    })
-    await axios(`${weatherURL}${chicago}&days=1&aqi=yes&alerts=no`) //                            Everything will be inside of apiRes.data!!!!!!!!!!!
-    // render the results on a 'show' page: aka 'detail' page
-    .then(apiRes => {
-        let weather = apiRes.data
-        let locName = weather.location.name
-        homelocName.push(locName)
-        let tempF = weather.current.temp_f
-        homeTemp.push(tempF)
-        let condition = weather.current.condition.text
-        homeCondition.push(condition)
-        // console.log('3 This is the Home Name: \n', homelocName,'3 This is the Temp Name: \n', homeTemp,'3 This is the Cindition Name: \n', homeCondition)
-    })
-    await axios(`${weatherURL}${houston}&days=1&aqi=yes&alerts=no`) //                            Everything will be inside of apiRes.data!!!!!!!!!!!
-    // render the results on a 'show' page: aka 'detail' page
-    .then(apiRes => {
-        let weather = apiRes.data
-        let locName = weather.location.name
-        homelocName.push(locName)
-        let tempF = weather.current.temp_f
-        homeTemp.push(tempF)
-        let condition = weather.current.condition.text
-        homeCondition.push(condition)
-        // console.log('4 This is the Home Name: \n', homelocName,'4 This is the Temp Name: \n', homeTemp,'4 This is the Cindition Name: \n', homeCondition)
-    })
-    await axios(`${weatherURL}${phoenix}&days=1&aqi=yes&alerts=no`) //                            Everything will be inside of apiRes.data!!!!!!!!!!!
-    // render the results on a 'show' page: aka 'detail' page
-    .then(apiRes => {
-        let weather = apiRes.data
-        let locName = weather.location.name
-        homelocName.push(locName)
-        let tempF = weather.current.temp_f
-        homeTemp.push(tempF)
-        let condition = weather.current.condition.text
-        homeCondition.push(condition)
-        // console.log('5 This is the Home Name: \n', homelocName,'5 This is the Temp Name: \n', homeTemp,'5 This is the Cindition Name: \n', homeCondition)
-    })
-    await axios(`${weatherURL}${philadelphia}&days=1&aqi=yes&alerts=no`) //                            Everything will be inside of apiRes.data!!!!!!!!!!!
-    // render the results on a 'show' page: aka 'detail' page
-    .then(apiRes => {
-        let weather = apiRes.data
-        let locName = weather.location.name
-        homelocName.push(locName)
-        let tempF = weather.current.temp_f
-        homeTemp.push(tempF)
-        let condition = weather.current.condition.text
-        homeCondition.push(condition)
-        // console.log('6 This is the Home Name: \n', homelocName,'6 This is the Temp Name: \n', homeTemp,'6 This is the Cindition Name: \n', homeCondition)
-    })
-    await axios(`${weatherURL}${sanAntonio}&days=1&aqi=yes&alerts=no`) //                            Everything will be inside of apiRes.data!!!!!!!!!!!
-    // render the results on a 'show' page: aka 'detail' page
-    .then(apiRes => {
-        let weather = apiRes.data
-        let locName = weather.location.name
-        homelocName.push(locName)
-        let tempF = weather.current.temp_f
-        homeTemp.push(tempF)
-        let condition = weather.current.condition.text
-        homeCondition.push(condition)
-        // console.log('7 This is the Home Name: \n', homelocName,'7 This is the Temp Name: \n', homeTemp,'7 This is the Cindition Name: \n', homeCondition)
-    })
-    await axios(`${weatherURL}${sanDiego}&days=1&aqi=yes&alerts=no`) //                            Everything will be inside of apiRes.data!!!!!!!!!!!
-    // render the results on a 'show' page: aka 'detail' page
-    .then(apiRes => {
-        let weather = apiRes.data
-        let locName = weather.location.name
-        homelocName.push(locName)
-        let tempF = weather.current.temp_f
-        homeTemp.push(tempF)
-        let condition = weather.current.condition.text
-        homeCondition.push(condition)
-        // console.log('8 This is the Home Name: \n', homelocName,'8 This is the Temp Name: \n', homeTemp,'8 This is the Cindition Name: \n', homeCondition)
-    })
-    await axios(`${weatherURL}${dallas}&days=1&aqi=yes&alerts=no`) //                            Everything will be inside of apiRes.data!!!!!!!!!!!
-    // render the results on a 'show' page: aka 'detail' page
-    .then(apiRes => {
-        let weather = apiRes.data
-        let locName = weather.location.name
-        homelocName.push(locName)
-        let tempF = weather.current.temp_f
-        homeTemp.push(tempF)
-        let condition = weather.current.condition.text
-        homeCondition.push(condition)
-        // console.log('9 This is the Home Name: \n', homelocName,'9 This is the Temp Name: \n', homeTemp,'9 This is the Cindition Name: \n', homeCondition)
-    })
-    await axios(`${weatherURL}${sanJose}&days=1&aqi=yes&alerts=no`) //                            Everything will be inside of apiRes.data!!!!!!!!!!!
-    // render the results on a 'show' page: aka 'detail' page
-    .then(apiRes => {
-        let weather = apiRes.data
-        let locName = weather.location.name
-        homelocName.push(locName)
-        let tempF = weather.current.temp_f
-        homeTemp.push(tempF)
-        let condition = weather.current.condition.text
-        homeCondition.push(condition)
-        // console.log('10 This is the Home Name: \n', homelocName,'10 This is the Temp Name: \n', homeTemp,'10 This is the Cindition Name: \n', homeCondition)
-        res.render('index', { title: 'Express', weather, homelocName, homeTemp, homeCondition, username, loggedIn, userId})
-    })
+    let coords = req.params
+    axios(`${weatherURL}${lat},${long}&days=3&aqi=yes&alerts=no`) //                            Everything will be inside of apiRes.data!!!!!!!!!!!
+        // render the results on a 'show' page: aka 'detail' page
+        .then(apiRes => {
+            let weather = apiRes.data
+            let locName = weather.location.name
+            let forecast1 = weather.forecast.forecastday[0] // DAY OF
+            let forecast2 = weather.forecast.forecastday[1] // TOMORROW
+            let forecast3 = weather.forecast.forecastday[2] // TOMORROWs TOMOROW
+            let locRegion = weather.location.region
+            let locTime = weather.location.localtime
+            let hours = forecast1.hour
+            // THis will be the time we start from
+            let localTime = locTime[11] + locTime[12]
+            let hourTime = hours.splice(localTime)
+            let idx = 0;
+
+            while (hourTime.length < 12) {
+                hourTime.push(forecast2.hour[idx])
+                idx++
+            }
+            let hour = hourTime[0]
+            let hour1 = hourTime[1]
+            let hour2 = hourTime[2]
+            let hour3 = hourTime[3]
+            let hour4 = hourTime[4]
+            let hour5 = hourTime[5]
+            let hour6 = hourTime[6]
+            let hour7 = hourTime[7]
+            let hour8 = hourTime[8]
+            let hour9 = hourTime[9]
+            let hour10 = hourTime[10]
+            let hour11 = hourTime[11]
+            //TESTING
+            console.log(hour7.time)
+            console.log(hour8.time)
+            console.log(hour9.time)
+            // console.log('7 8 9 not working: \n', hourTime)
+            //FIRST
+            let newString = null;
+            let month;
+            let dayco = forecast1.date[8] += forecast1.date[9];
+            let dayConversion = dayco;
+            let monco = forecast1.date[5] += forecast1.date[6];
+            let monthConversion = monco.toString();
+            if (monthConversion.indexOf("-") >= 0) {
+                newString = monthConversion.replace("-", "");
+            }
+            if (newString) {
+                if (newString === 1) {
+                    month = "Jan";
+                } else if (newString === 2) {
+                    month = "Feb";
+                } else if (newString === 3) {
+                    month = "Mar";
+                } else if (newString === 4) {
+                    month = "Apr";
+                } else if (newString === 5) {
+                    month = "May";
+                } else if (newString === 6) {
+                    month = "June";
+                } else if (newString === 7) {
+                    month = "July";
+                } else if (newString === 8) {
+                    month = "Aug";
+                } else if (newString === 9) {
+                    month = "Sept";
+                } else if (newString === 10) {
+                    month = "Oct";
+                } else if (newString === 11) {
+                    month = "Nov";
+                } else {
+                    month = "Dec";
+                }
+            } else if (monthConversion === 1) {
+                month = "Jan";
+            } else if (monthConversion === 2) {
+                month = "Feb";
+            } else if (monthConversion === 3) {
+                month = "Mar";
+            } else if (monthConversion === 4) {
+                month = "Apr";
+            } else if (monthConversion === 5) {
+                month = "May";
+            } else if (monthConversion === 6) {
+                month = "June";
+            } else if (monthConversion === 7) {
+                month = "July";
+            } else if (monthConversion === 8) {
+                month = "Aug";
+            } else if (monthConversion === 9) {
+                month = "Sept";
+            } else if (monthConversion === 10) {
+                month = "Oct";
+            } else if (monthConversion === 11) {
+                month = "Nov";
+            } else {
+                month = "Dec";
+            }
+            console.log('This is the month: \n', month)
+            //SECOND
+            let newString1 = null;
+            let month1;
+            let dayco1 = forecast2.date[8] += forecast2.date[9];
+            let dayConversion1 = dayco1;
+            let monco1 = forecast2.date[5] += forecast2.date[6];
+            let monthConversion1 = monco1.toString();
+            if (monthConversion1.indexOf("-") >= 0) {
+                newString1 = monthConversion1.replace("-", "");
+            }
+            if (newString1) {
+                if (newString1 === 1) {
+                    month1 = "Jan";
+                } else if (newString1 === 2) {
+                    month1 = "Feb";
+                } else if (newString1 === 3) {
+                    month1 = "Mar";
+                } else if (newString1 === 4) {
+                    month1 = "Apr";
+                } else if (newString1 === 5) {
+                    month1 = "May";
+                } else if (newString1 === 6) {
+                    month1 = "June";
+                } else if (newString1 === 7) {
+                    month1 = "July";
+                } else if (newString1 === 8) {
+                    month1 = "Aug";
+                } else if (newString1 === 9) {
+                    month1 = "Sept";
+                } else if (newString1 === 10) {
+                    month1 = "Oct";
+                } else if (newString1 === 11) {
+                    month1 = "Nov";
+                } else {
+                    month1 = "Dec";
+                }
+            } else if (monthConversion1 === 1) {
+                month1 = "Jan";
+            } else if (monthConversion1 === 2) {
+                month1 = "Feb";
+            } else if (monthConversion1 === 3) {
+                month1 = "Mar";
+            } else if (monthConversion1 === 4) {
+                month1 = "Apr";
+            } else if (monthConversion1 === 5) {
+                month1 = "May";
+            } else if (monthConversion1 === 6) {
+                month1 = "June";
+            } else if (monthConversion1 === 7) {
+                month1 = "July";
+            } else if (monthConversion1 === 8) {
+                month1 = "Aug";
+            } else if (monthConversion1 === 9) {
+                month1 = "Sept";
+            } else if (monthConversion1 === 10) {
+                month1 = "Oct";
+            } else if (monthConversion1 === 11) {
+                month1 = "Nov";
+            } else {
+                month1 = "Dec";
+            }
+            console.log(month1)
+            //THIRD
+            let newString2 = null;
+            let month2;
+            let dayco2 = forecast2.date[8] += forecast2.date[9];
+            let dayConversion2 = dayco2;
+            let monco2 = forecast3.date[5] += forecast3.date[6];
+            let monthConversion2 = monco2.toString();
+            if (monthConversion2.indexOf("-") >= 0) {
+                newString2 = monthConversion2.replace("-", "");
+            }
+            if (newString2) {
+                if (newString2 === 1) {
+                    month2 = "Jan";
+                } else if (newString2 === 2) {
+                    month2 = "Feb";
+                } else if (newString2 === 3) {
+                    month2 = "Mar";
+                } else if (newString2 === 4) {
+                    month2 = "Apr";
+                } else if (newString2 === 5) {
+                    month2 = "May";
+                } else if (newString2 === 6) {
+                    month2 = "June";
+                } else if (newString2 === 7) {
+                    month2 = "July";
+                } else if (newString2 === 8) {
+                    month2 = "Aug";
+                } else if (newString2 === 9) {
+                    month2 = "Sept";
+                } else if (newString2 === 10) {
+                    month2 = "Oct";
+                } else if (newString2 === 11) {
+                    month2 = "Nov";
+                } else {
+                    month2 = "Dec";
+                }
+            } else if (monthConversion2 === 1) {
+                month2 = "Jan";
+            } else if (monthConversion2 === 2) {
+                month2 = "Feb";
+            } else if (monthConversion2 === 3) {
+                month2 = "Mar";
+            } else if (monthConversion2 === 4) {
+                month2 = "Apr";
+            } else if (monthConversion2 === 5) {
+                month2 = "May";
+            } else if (monthConversion2 === 6) {
+                month2 = "June";
+            } else if (monthConversion2 === 7) {
+                month2 = "July";
+            } else if (monthConversion2 === 8) {
+                month2 = "Aug";
+            } else if (monthConversion2 === 9) {
+                month2 = "Sept";
+            } else if (monthConversion2 === 10) {
+                month2 = "Oct";
+            } else if (monthConversion2 === 11) {
+                month2 = "Nov";
+            } else {
+                month2 = "Dec";
+            }
+            console.log(dayConversion2)
+            res.render('weather/daily', { weather, hour, hour1, hour2, hour3, hour4, hour5, hour6, hour7, hour8, hour9, hour10, hour11, forecast1, forecast2, forecast3, month1, month2, dayConversion1, dayConversion2, username, loggedIn, userId})
+        })
         .catch(err => {
             console.log('error', err)
             res.redirect(`/error?error=${err}`)
     })
 })
-
 
 
 // This will be for the hourly forcasts with a ZIP CODE INSTEAD !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -21,93 +21,93 @@ async function initMap() {
         mapTypeControl: false,
     });
 
-geocoder = new google.maps.Geocoder();
+    geocoder = new google.maps.Geocoder();
 
-const inputText = document.createElement("input");
+    const inputText = document.createElement("input");
 
-inputText.type = "text";
-inputText.placeholder = "Enter a location";
+    inputText.type = "text";
+    inputText.placeholder = "Enter a location";
 
-const submitButton = document.createElement("input");
+    const submitButton = document.createElement("input");
 
-submitButton.type = "button";
-submitButton.value = "Geocode";
-submitButton.classList.add("button", "button-primary");
+    submitButton.type = "button";
+    submitButton.value = "Geocode";
+    submitButton.classList.add("button", "button-primary");
 
-const clearButton = document.createElement("input");
+    const clearButton = document.createElement("input");
 
-clearButton.type = "button";
-clearButton.value = "Clear";
-clearButton.classList.add("button", "button-secondary");
-response = document.createElement("pre");
-response.id = "response";
-response.innerText = "";
-responseDiv = document.createElement("div");
-responseDiv.id = "response-container";
-responseDiv.appendChild(response);
-//                                                                                               response IS the object   obj>location> lat: / lng:
-// console.log('This is the response: \n', response);
+    clearButton.type = "button";
+    clearButton.value = "Clear";
+    clearButton.classList.add("button", "button-secondary");
+    response = document.createElement("pre");
+    response.id = "response";
+    response.innerText = "";
+    responseDiv = document.createElement("div");
+    responseDiv.id = "response-container";
+    responseDiv.appendChild(response);
+    //                                                                                               response IS the object   obj>location> lat: / lng:
+    // console.log('This is the response: \n', response);
 
-const instructionsElement = document.createElement("p");
+    const instructionsElement = document.createElement("p");
 
-instructionsElement.id = "instructions";
-instructionsElement.innerHTML =
-"<strong>Instructions</strong>: Enter an address in the textbox to geocode or click on the map to reverse geocode.";
-map.controls[google.maps.ControlPosition.TOP_LEFT].push(inputText);
-map.controls[google.maps.ControlPosition.TOP_LEFT].push(submitButton);
-map.controls[google.maps.ControlPosition.TOP_LEFT].push(clearButton);
-// map.controls[google.maps.ControlPosition.LEFT_TOP].push(instructionsElement);
-//                                                                                               This was the instructions, it was blocking the view^^^^^^
-// map.controls[google.maps.ControlPosition.LEFT_TOP].push(responseDiv);
-//                                                                                                  THIS WAS THE WINDOW THAT POPPS UP WITH THE OBJECT!!!!!^^^^^^
-marker = new google.maps.Marker({
-map,
-});
-map.addListener("click", (e) => {
-geocode({ location: e.latLng });
-});
-submitButton.addEventListener("click", () =>
-geocode({ address: inputText.value }),
-);
-clearButton.addEventListener("click", () => {
-clear();
-});
-clear();
-}
+    instructionsElement.id = "instructions";
+    instructionsElement.innerHTML =
+    "<strong>Instructions</strong>: Enter an address in the textbox to geocode or click on the map to reverse geocode.";
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(inputText);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(submitButton);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(clearButton);
+    // map.controls[google.maps.ControlPosition.LEFT_TOP].push(instructionsElement);
+    //                                                                                               This was the instructions, it was blocking the view^^^^^^
+    // map.controls[google.maps.ControlPosition.LEFT_TOP].push(responseDiv);
+    //                                                                                                  THIS WAS THE WINDOW THAT POPPS UP WITH THE OBJECT!!!!!^^^^^^
+    marker = new google.maps.Marker({
+    map,
+    });
+    map.addListener("click", (e) => {
+    geocode({ location: e.latLng });
+    });
+    submitButton.addEventListener("click", () =>
+    geocode({ address: inputText.value }),
+    );
+    clearButton.addEventListener("click", () => {
+    clear();
+    });
+    clear();
+    }
 
-function clear() {
-marker.setMap(null);
-responseDiv.style.display = "none";
-}
+    function clear() {
+    marker.setMap(null);
+    responseDiv.style.display = "none";
+    }
 
-function geocode(request) {
-clear();
-geocoder
-.geocode(request)
-.then((result) => {
-const { results } = result;
+    function geocode(request) {
+    clear();
+    geocoder
+    .geocode(request)
+    .then((result) => {
+    const { results } = result;
 
-map.setCenter(results[0].geometry.location);
-marker.setPosition(results[0].geometry.location);
-marker.setMap(map);
-responseDiv.style.display = "block";
-response.innerText = JSON.stringify(result, null, 2);
-// THis is my stuff
-data.push(results);
-lat = data[0][0].geometry.viewport.eb.lo;
-long = data[0][0].geometry.viewport.La.hi;
-lat = lat.toString();
-long = long.toString();
-latlong = lat + ',' + long;
+    map.setCenter(results[0].geometry.location);
+    marker.setPosition(results[0].geometry.location);
+    marker.setMap(map);
+    responseDiv.style.display = "block";
+    response.innerText = JSON.stringify(result, null, 2);
+    // THis is my stuff
+    data.push(results);
+    lat = data[0][0].geometry.viewport.eb.lo;
+    long = data[0][0].geometry.viewport.La.hi;
+    lat = lat.toString();
+    long = long.toString();
+    latlong = lat + ',' + long;
 
-coords.innerHTML += `<p>${latlong}</p>`
-// END OF MY SATUFF
-return results;
-})
-.catch((e) => {
-alert("Geocode was not successful for the following reason: " + e);
-});
-// THis is my stuff
+    coords.innerHTML += `<a class="btn btn-primary" href="weather/daily/${lat},${long}">Lets Go!</a>`
+    // END OF MY SATUFF
+    return results;
+    })
+    .catch((e) => {
+    alert("Geocode was not successful for the following reason: " + e);
+    });
+    // THis is my stuff
 }
 
 
